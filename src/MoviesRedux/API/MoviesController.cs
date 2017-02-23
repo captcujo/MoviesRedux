daa38c79a30ebaf414aaf609c4a4e5699039c881
Matthew Ploor <mploor@cox.net>
2017-02-23 10:40:50 -0700

    Added HttpGet to MoviesController

--- src/MoviesRedux/API/MoviesController.cs
+++ src/MoviesRedux/API/MoviesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MoviesRedux.Data;
+using MoviesRedux.Models;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,5 +13,19 @@ namespace MoviesRedux.API
     [Route("api/[controller]")]
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _db;
+
+        public MoviesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public List<Movie> Get()
+        {
+            List<Movie> movs = (from m in _db.Movies
+                                select m).ToList();
+            return movs;
+        }
     }
 }
